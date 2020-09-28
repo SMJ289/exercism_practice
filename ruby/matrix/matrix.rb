1fdaa26ce5f692947b659b9680e7f27b7a88b598
Steven Meyers <sjjmeyers@gmail.com>
2020-09-28 10:54:21 -0500

    Solve matrix

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,5 +1,19 @@
+require 'pry'
 class Matrix
-  def initialize
 
+  def initialize(data)
+    @string_matrix = data
+  end
+
+  def rows
+    split_string = @string_matrix.split("\n")
+    second_split = split_string.map do |row|
+      row.split(" ")
+    end
+    integers = second_split.map {|row| row.map {|char| char.to_i}}
+  end
+
+  def columns
+    self.rows.transpose
   end
 end
