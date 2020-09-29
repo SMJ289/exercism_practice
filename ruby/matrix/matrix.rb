f0a3384114417ff1924f8a8449bae5218c5d0b00
Steven Meyers <sjjmeyers@gmail.com>
2020-09-29 16:40:50 -0500

    Format

--- ruby/matrix/matrix.rb
+++ ruby/matrix/matrix.rb
@@ -1,19 +1,17 @@
 require 'pry'
 class Matrix
-
   def initialize(data)
     @string_matrix = data
   end
 
   def rows
     split_string = @string_matrix.split("\n")
-    second_split = split_string.map do |row|
-      row.split(" ")
+    split_string.map do |row|
+      row.split(' ').map(&:to_i)
     end
-    integers = second_split.map {|row| row.map {|char| char.to_i}}
   end
 
   def columns
-    self.rows.transpose
+    rows.transpose
   end
 end
