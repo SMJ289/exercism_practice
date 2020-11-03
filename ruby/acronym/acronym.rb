54dc76a324e664651a482796c1176a9b075f124b
Steven Meyers <sjjmeyers@gmail.com>
2020-11-03 10:18:40 -0600

    Solve acronym

--- ruby/acronym/acronym.rb
+++ ruby/acronym/acronym.rb
@@ -1,8 +1,12 @@
-=begin
-Write your code for the 'Acronym' exercise in this file. Make the tests in
-`acronym_test.rb` pass.
+class Acronym
+  def self.abbreviate(term)
+    split_term = term.split(/\W/) - [""]
+    acronym = split_term.map do |word|
+      word.slice(0).capitalize
+    end
+    acronym.join
+  end
 
-To get started with TDD, see the `README.md` file in your
-`ruby/acronym` directory.
-=end
+end
 
+# \w \d \s
